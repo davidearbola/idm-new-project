2792883a58749a2ce7440daf60dc1efa4d34e8cd
DavideArbola <62762302+davidearbola@users.noreply.github.com>
2025-07-16 15:06:54 +0200

    minor fix

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,6 @@
 <script setup>
-import { RouterLink, RouterView, useRouter } from 'vue-router'
-import { useAuthStore } from '@/stores/authStore'
-import { storeToRefs } from 'pinia'
-import { useToast } from 'vue-toastification';
 import HeaderPublic from './components/HeaderPublic.vue'
 import FooterPublic from './components/FooterPublic.vue'
-
-const toast = useToast();
-const router = useRouter()
-const authStore = useAuthStore()
-const { user } = storeToRefs(authStore)
-
-const handleLogout = async () => {
-  const response = await authStore.logout();
-  
-  if (response.success) {
-    toast.success(response.message);
-    router.push({ name: 'login' });
-  } else {
-    toast.error(response.message);
-  }
-};
 
 </script>
 
